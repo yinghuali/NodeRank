d48ad9272c7866faa5aa067fed12cbc42fa91226
yinghuali <yinghua.li@uni.lu>
2023-10-24 08:49:19 +0200

    update

--- mutation_node_edge.sh
+++ mutation_node_edge.sh
@@ -1,5 +1,12 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=1-23:00:00
+#SBATCH --mail-type=end,fail
+#SBATCH --mail-user=yinghua.li@uni.lu
+#SBATCH -p batch
+#SBATCH --mem 100G
 
-# mutation_node_edge
 python mutation_node_edge.py --path_edge_index_np 'data/cora/edge_index_np.pkl' --n_edges 50 --save_path_pkl 'data/cora/mutation_edge_index_np_list_50.pkl'
 python mutation_node_edge.py --path_edge_index_np 'data/cora/edge_index_np.pkl' --n_edges 100 --save_path_pkl 'data/cora/mutation_edge_index_np_list_100.pkl'
 python mutation_node_edge.py --path_edge_index_np 'data/cora/edge_index_np.pkl' --n_edges 150 --save_path_pkl 'data/cora/mutation_edge_index_np_list_150.pkl'
@@ -20,7 +27,5 @@ python mutation_node_edge.py --path_edge_index_np 'data/pubmed/edge_index_np.pkl
 python mutation_node_edge.py --path_edge_index_np 'data/pubmed/edge_index_np.pkl' --n_edges 150 --save_path_pkl 'data/pubmed/mutation_edge_index_np_list_150.pkl'
 python mutation_node_edge.py --path_edge_index_np 'data/pubmed/edge_index_np.pkl' --n_edges 200 --save_path_pkl 'data/pubmed/mutation_edge_index_np_list_200.pkl'
 
-
-#
 
 
