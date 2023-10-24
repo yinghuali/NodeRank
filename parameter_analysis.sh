98d996bfa36e0ab252372350486066a7b1a312d3
yinghuali <yinghua.li@uni.lu>
2023-10-24 16:18:51 +0200

    update

---
+++ parameter_analysis.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=1-23:00:00
+#SBATCH --mail-type=end,fail
+#SBATCH --mail-user=yinghua.li@uni.lu
+#SBATCH -p batch
+#SBATCH --mem 100G
+
